fbfca36f00ff435d1b18c49dbff67d7a4adc67a6
JoaoVictor77703 <joao@nurog.com.br>
2023-03-16 22:12:11 -0300

    Alguma Coisa

---
+++ app/src/main/java/br/com/etecia/cineema/Activity_Splash.java
@@ -0,0 +1,14 @@
+package br.com.etecia.cineema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+    }
+}
